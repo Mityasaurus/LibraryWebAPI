2b730a23597b0d0b2898bd2ee741e4404efc73de
Mityasaurus <dmitriybovdun5@gmail.com>
2024-09-16 20:39:54 +0300

    Add data access layer

---
+++ LibraryWebAPI.DataAccess/Installers/DataInstaller.cs
@@ -0,0 +1,19 @@
+﻿using LibraryWebAPI.DataAccess.Repositories.Author;
+using LibraryWebAPI.DataAccess.Repositories.Book;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryWebAPI.DataAccess.Installers
+{
+    public static class DataInstaller
+    {
+        public static IServiceCollection AddDataContext(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<LibraryContext>()
+                .AddTransient<IAuthorRepository, AuthorRepository>()
+                .AddTransient<IBookRepository, BookRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ LibraryWebAPI.DataAccess/Repositories/Book/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryWebAPI.DataAccess.Entities;
+using System.Collections.ObjectModel;
+
+namespace LibraryWebAPI.DataAccess.Repositories.Book
+{
+    public interface IBookRepository
+    {
+        public Task<ReadOnlyCollection<BookEntity>> GetAll();
+        public Task<BookEntity?> Get(string id);
+        public Task<ReadOnlyCollection<BookEntity>> Get(Func<BookEntity, bool> predicate);
+        public Task Create(BookEntity entity);
+        public Task Update(BookEntity entity);
+        public Task Delete(string id);
+    }
+}
