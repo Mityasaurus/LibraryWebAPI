4a3d05884db89a9b7f7fc4a6e14e06f3a5da6804
Mityasaurus <dmitriybovdun5@gmail.com>
2024-09-20 20:18:06 +0300

    Add dtos

---
+++ LibraryWebAPI.BusinessLogic/Dtos/AuthorDto.cs
@@ -0,0 +1,31 @@
+﻿namespace LibraryWebAPI.BusinessLogic.Dtos
+{
+    public class AuthorDto : IEquatable<AuthorDto>
+    {
+        public static readonly AuthorDto Default =
+            new (string.Empty, string.Empty, string.Empty);
+
+        public string Id { get; }
+        public string Name { get; }
+        public string Lastname { get; }
+
+        public AuthorDto(string id, string name, string lastname) 
+        { 
+            Id = id;
+            Name = name;
+            Lastname = lastname;
+        }
+
+        public bool Equals(AuthorDto? other)
+        {
+            if(other == null) return false;
+
+            return Id == other.Id && Name == other.Name && Lastname == other.Lastname;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Lastname);
+        }
+    }
+}
